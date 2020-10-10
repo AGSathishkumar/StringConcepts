1b40400c39ee973127f5e8bf244e2fa5855f4b5b
kumar <satinfo@25gmail.com>
2020-10-10 11:30:31 +0530

    Create a new String

---
+++ src/String_Constructor.java
@@ -0,0 +1,21 @@
+
+public class String_Constructor {
+
+	public static void main(String args[])
+
+	{
+		// Creating a String
+		
+		// 1. Literal 
+		
+		String literal = "Welcome";
+		
+		System.out.println("Literal	"+literal);
+		// 2.New
+		String var = new String("Welcome");
+		System.out.println("New	"+var);
+
+	}
+}
+
+
