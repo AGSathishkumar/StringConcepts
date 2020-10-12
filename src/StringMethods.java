652509e0bf0b4773ff34335768834f17fc746d65
kumar <satinfo@25gmail.com>
2020-10-12 10:04:17 +0530

    Updated methods in code

---
+++ src/StringMethods.java
@@ -0,0 +1,112 @@
+
+public class StringMethods {
+
+	public static void main(String args[]) {
+		System.out.println("*******************String Methods*******************");
+
+		// 1. int length
+
+		String sampleText = "Welcome to Learn";
+
+		int length = sampleText.length();
+
+		System.out.println("1.) Length --> " + length);
+
+		// 2. Char charAt(int i)
+
+		char charAt = sampleText.charAt(3);
+
+		System.out.println("2.) CharAt --> " + charAt);
+
+		// 3. String substring(int i)
+
+		String substring = sampleText.substring(2);
+
+		System.out.println("3.) substring --> " + substring);
+
+		// 4. String substring(int i,int j)
+
+		String substring1 = sampleText.substring(0, 4);
+
+		System.out.println("4.) substring(int i,intj) --> " + substring1);
+
+		// 5. String concat(String str)
+
+		String concat = sampleText.concat("Learning");
+
+		System.out.println("5.) Concat --> " + concat);
+
+		// 6.indexOf (String s)
+
+		int indexOf = sampleText.indexOf("Learn");
+
+		System.out.println("6.) indexOf --> " + indexOf);
+
+		// 7.indexOf (String s,int i)
+
+		int indexOf_second = sampleText.indexOf('L', 1);
+
+		System.out.println("7.) indexOf (String s,int i) --> " + indexOf_second);
+
+		// 8. lastindexOf(String s)
+
+		int lastIndexOf = sampleText.lastIndexOf('e');
+
+		System.out.println("8.) lastindexOf --> " + lastIndexOf);
+
+		// 9. boolean equals( Object otherObj) (Entire word)
+
+		boolean output = sampleText.equals("Welcome to Learn");
+
+		System.out.println("9.) equals --> " + output);
+
+		// 10. boolean equalsIgnoreCase( Object otherObj) (Entire word)
+
+		boolean output1 = sampleText.equalsIgnoreCase("Welcome to learn");
+
+		System.out.println("10.) equalsIgnoreCase --> " + output1);
+
+//		// 11. int compareTo( String anotherString)
+//		
+//		int compareTo = sampleText.compareTo("to");
+//		
+//		System.out.println("11.) compareTo --> "+compareTo);
+
+//		// 12. int compareToIgnoreCase( String anotherString)
+
+		// 13.String toLowerCase
+
+		String lowerCase = sampleText.toLowerCase();
+
+		System.out.println("13.) toLowerCase --> " + lowerCase);
+
+		// 14.String toUpperCase()
+
+		String upperCase = sampleText.toUpperCase();
+
+		System.out.println("14.) toUpperCase --> " + upperCase);
+
+		// 15.String trim
+
+		String sampleText1 = " Text ";
+
+		String trim = sampleText1.trim();
+
+		System.out.println("15.) trim -->" + trim);
+		
+		// 16. String replace (char oldChar, char newChar) ---> String
+		
+		String replace = sampleText.replace("We","A");
+		
+		System.out.println("16.) (char oldChar, char newChar) --> "+replace);
+		
+		// 16. String replace (char oldChar, char newChar) ---> Character
+		
+		String replace1 = sampleText.replace('W','A');
+		
+		System.out.println("17.) (char oldChar, char newChar) --> "+replace1);
+		
+
+
+	}
+}
--- src/String_Constructor.java
+++ src/String_Constructor.java
@@ -6,122 +6,117 @@ public class String_Constructor {
 	public static void main(String args[]) throws UnsupportedEncodingException
 
 	{
-		//******************** Creating a String ******************** 
+		// ******************** Creating a String ********************
 		System.out.println("******************** Creating a String *********************\n ");
-		
-		// 1. Literal 
-		
+
+		// 1. Literal
+
 		String literal = "Welcome";
-		
-		System.out.println("Literal --> "+literal);
-		
+
+		System.out.println("Literal --> " + literal);
+
 		// 2.New
-		
+
 		String var = new String("Welcome");
-		
-		System.out.println("New	--> "+var+"\n");
-		//******************** String Constructor ******************** 
+
+		System.out.println("New	--> " + var + "\n");
+		// ******************** String Constructor ********************
 
 		System.out.println("******************** String Constructor ********************\n");
-		
+
 		// 1.String (byte[] arr)
-		
-		byte[] b_arr = {71, 101, 101, 107, 115};
-		
-		String output= new String(b_arr);
-		
-		System.out.println("1.) String (byte[] arr) --> "+output);
-		
+
+		byte[] b_arr = { 71, 101, 101, 107, 115 };
+
+		String output = new String(b_arr);
+
+		System.out.println("1.) String (byte[] arr) --> " + output);
+
 		// 2.String (byte[] arr,Charset char_set)
-		
-		byte[] b_ar = {71, 101, 101, 107, 115};
-		
+
+		byte[] b_ar = { 71, 101, 101, 107, 115 };
+
 		Charset cs = Charset.defaultCharset();
-		
-		System.out.println("Charset --> "+cs);
 
-		String s_byte_char = new String(b_arr, cs); //Geeks
+		System.out.println("Charset --> " + cs);
 
-		System.out.println("2.) String (byte[] arr,Charset char_set) --> " +s_byte_char);
-		
+		String s_byte_char = new String(b_arr, cs); // Geeks
+
+		System.out.println("2.) String (byte[] arr,Charset char_set) --> " + s_byte_char);
+
 		// 3.String (byte[] b,String charsetName)
-		
-		byte[] b = {71, 101, 101, 107, 115};
-		
-		String op=new String(b,"US-ASCII");
-		
-		System.out.println("3.) String (byte[] b,String charsetName) --> " +op);
+
+		byte[] b = { 71, 101, 101, 107, 115 };
+
+		String op = new String(b, "US-ASCII");
+
+		System.out.println("3.) String (byte[] b,String charsetName) --> " + op);
 
 		// 4.String (byte[] a,int startindex,int length)
-		
-		byte[] a = {71, 101, 101, 107, 115};
 
-		String output1=new String (a,1,3);
-		
-		System.out.println("4.) String (byte[] a,int startindex,int length) -->" +output1);
-		
+		byte[] a = { 71, 101, 101, 107, 115 };
+
+		String output1 = new String(a, 1, 3);
+
+		System.out.println("4.) String (byte[] a,int startindex,int length) -->" + output1);
+
 		// 5.String (byte[] c,int startindex,int length,Charset charset)
-		
-		byte[] c = {71, 101, 101, 107, 115};
-		
-		Charset cs1=Charset.defaultCharset();
-		
-		String output2= new String(c,1,3,cs1);
-			
-		System.out.println("5.) String (byte[] c,int startindex,int length,Charset charset) -->" +output2);
+
+		byte[] c = { 71, 101, 101, 107, 115 };
+
+		Charset cs1 = Charset.defaultCharset();
+
+		String output2 = new String(c, 1, 3, cs1);
+
+		System.out.println("5.) String (byte[] c,int startindex,int length,Charset charset) -->" + output2);
 
 		// 6.String (byte[] d,int startindex,int length,String charsetname)
-		
-		byte[] d = {71, 101, 101, 107, 115};
-				
-		String output3= new String(c,1,4,"US-ASCII");
-			
-		System.out.println("6.) String (byte[] d,int startindex,int length,String charsetname) -->" +output3);
+
+		byte[] d = { 71, 101, 101, 107, 115 };
+
+		String output3 = new String(c, 1, 4, "US-ASCII");
+
+		System.out.println("6.) String (byte[] d,int startindex,int length,String charsetname) -->" + output3);
 
 		// 7.String (char[] char_arr)
-		
-		char char_arr[] = {'G', 'e', 'e', 'k', 's'};
 
-		String output4=new String(char_arr);
+		char char_arr[] = { 'G', 'e', 'e', 'k', 's' };
 
-		System.out.println("7.) String (char[] char_arr) -->" +output4);
+		String output4 = new String(char_arr);
+
+		System.out.println("7.) String (char[] char_arr) -->" + output4);
 
 		// 8.String (char[] e,int start_index,int count)
-		
-		char e[] = {'G', 'e', 'e', 'k', 's'};
 
-		String output5 = new String(e , 1, 3); 
+		char e[] = { 'G', 'e', 'e', 'k', 's' };
 
-		System.out.println("8.) String (char[] e,int start_index,int count) -->" +output5);
+		String output5 = new String(e, 1, 3);
 
-		// 9.String(int[] uni_code_points, int offset, int count) 
+		System.out.println("8.) String (char[] e,int start_index,int count) -->" + output5);
 
-		int[] uni_code = {71, 101, 101, 107, 115};
-		
-		String output6 = new String(uni_code, 1, 3); 
-		
-		System.out.println("9.) String(int[] uni_code_points, int offset, int count)  -->" +output6);
+		// 9.String(int[] uni_code_points, int offset, int count)
+
+		int[] uni_code = { 71, 101, 101, 107, 115 };
+
+		String output6 = new String(uni_code, 1, 3);
+
+		System.out.println("9.) String(int[] uni_code_points, int offset, int count)  -->" + output6);
 
 		// 10.String (StringBuffer s_buffer)
-		
+
 		StringBuffer s_buffer = new StringBuffer("Geeks");
 
-		String output7 = new String(s_buffer); //Geeks
-		
-		System.out.println("10.)String (StringBuffer s_buffer)  -->" +output7);
+		String output7 = new String(s_buffer); // Geeks
 
-		// 11.String (StringBuilder  sbuilder)
-		
+		System.out.println("10.)String (StringBuffer s_buffer)  -->" + output7);
+
+		// 11.String (StringBuilder sbuilder)
+
 		StringBuilder s_builder = new StringBuilder("Geeks");
 
 		String output8 = new String(s_builder);
-		
-		System.out.println("11.)String (StringBuilder  sbuilder)  --->"+output8);
 
-
-		
+		System.out.println("11.)String (StringBuilder  sbuilder)  --->" + output8);
 
 	}
 }
-
-
